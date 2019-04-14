6fc0cf5287f7722223b90deae10fc9bf03659b93
Michał Gorczyca <michgorc@gmail.com>
2019-04-14 10:39:27 +0200

    Create complex.hpp

---
+++ complex.hpp
@@ -0,0 +1,18 @@
+#ifndef COMPLEX1_HPP_
+#define COMPLEX1_HPP_
+
+class Complex {
+public:
+    Complex(double re, double im) : re_(re), im_(im) {}
+
+    // Użycie delegowania konstruktora.
+    Complex(double data[2]) : Complex(data[0], data[1]) {}
+
+    double re() const { return re_; }
+    double im() const { return im_; }
+private:
+    double re_;
+    double im_;
+};
+
+#endif
